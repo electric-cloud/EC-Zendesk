867a179df21032662d00a4f9d35e1d95cf538680
lrochette <lrochette@electric-cloud.com>
2015-05-22 22:00:34 -0700

    EC-Zendesk v1.0.0.71: Adding createConfiguration procedure

--- src/project/manifest.pl
+++ src/project/manifest.pl
@@ -1,5 +1,7 @@
 @files = (
 	['//project/propertySheet/property[propertyName="ec_setup"]/value', 'ec_setup.pl'],
 	['//project/procedure[procedureName="commentOnTicket"]/step[stepName="commentTicket"]/command', 'procedures/commentOnTicket/steps/commentTicket.pl'],
+	['//project/procedure[procedureName="createConfiguration"]/step[stepName="CreateConfiguration"]/command', 'procedures/createConfiguration/steps/CreateConfiguration.pl'],
+	['//project/procedure[procedureName="createConfiguration"]/step[stepName="CreateAndAttachCredential"]/command', 'procedures/createConfiguration/steps/CreateAndAttachCredential.pl'],
 	['//project/procedure[procedureName="createTicket"]/step[stepName="openTicket"]/command', 'procedures/createTicket/steps/openTicket.pl'],
 );
