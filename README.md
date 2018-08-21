4c22f4bed5697726eb1be248870ce6738a917a7e
Laurent Rochette <lrochette@eletric-cloud.com>
2018-08-20 20:19:07 -0500

    v1.4.0.149 - change Zendesk URL

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# EC-Zendesk
-A plugin to communicate with Zendesk
+# EC-Liquibase
+==============
+
+The ElectricFlow integration with Zendesk
+
+#### Prerequisite installation: ####
+
+1. EC-Admin is required
+
+## Installation
+
+`ectool installPlugin EC-Liquibase.jar`
+
+#### Prerequisite build: ####
+
+1. install and compile ecpluginbuilder
+2. the system expect it in the directory above your repository
+
+## Build ##
+1. `ec-perl ecpluginbuilder.pl`
+
+This will create a new version of the EC-Zendesk.jar and then install and
+promote it to the ElectricFlow server you are currently logged in.
---
+++ dsl/procedures/createTicket/procedure.groovy
@@ -0,0 +1,40 @@
+/* ###########################################################################
+# Main procedure to run Liquibase commands
+#
+# Author: L.Rochette
+#
+# Copyright 2018 Electric Cloud, Inc.
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+#
+# History
+# ---------------------------------------------------------------------------
+# 2018-Aug-17 lrochette Conversion to PLuginWizard
+#
+############################################################################ */
+import java.io.File
+
+def procName = 'createTicket'
+procedure procName,
+  description: 'Create a Zendesk Ticket',
+{
+  step 'checkConfiguration',
+    description: 'Verify the configuration exists',
+    command: new File(pluginDir, "dsl/procedures/$procName/steps/checkConfiguration.pl").text,
+    shell: 'ec-perl',
+    errorHandling: 'abortJob'
+
+  step 'createTicket',
+    command: new File(pluginDir, "dsl/procedures/$procName/steps/createTicket.pl").text,
+    shell: 'ec-perl'
+}
